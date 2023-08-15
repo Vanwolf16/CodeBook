51c03080aa59f5b6d0c500653c3cafdda21e3b98
vanwolf16 <d.murillo28@yahoo.com>
2023-08-15 12:41:12 -0700

    ProductList Added to the site

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Hero } from "./components/Hero";
 import { FeaturedProducts } from "./components/FeaturedProducts";
 import { Faq } from './components/Faq';
+import { Testimonials } from './components/Testimonial';
 
 export const HomePage = () => {
     return(
         <main>
             <Hero />
             <FeaturedProducts />
+            <Testimonials />
             <Faq />
         </main>
     )
