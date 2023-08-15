ef59d33b7d0b36814097e938bd86a44dad659a84
vanwolf16 <d.murillo28@yahoo.com>
2023-08-15 11:11:31 -0700

    HomePage Added

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Hero } from "./components/Hero";
+import { FeaturedProducts } from "./components/FeaturedProducts";
+import { Faq } from './components/Faq';
 
 export const HomePage = () => {
     return(
         <main>
-            <div>Home</div>
+            <Hero />
+            <FeaturedProducts />
+            <Faq />
         </main>
     )
 }
