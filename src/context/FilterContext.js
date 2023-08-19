246b720fec252b963543ff8d0ff75a0427e3cff1
vanwolf16 <d.murillo28@yahoo.com>
2023-08-19 14:02:02 -0700

    Filter Bar is all fixed

--- src/context/FilterContext.js
+++ src/context/FilterContext.js
@@ -23,8 +23,49 @@ export const FilterProvider = ({children}) => {
         });
     }
 
-    const value = {
-        products: state.productList,
+
+    function bestSeller(products){
+        return state.bestSellerOnly ? products.filter(product => product.best_seller === true) : products;
+    }
+
+    function inStock(products){
+        return state.onlyInStock ? products.filter(product => product.in_stock === true) : products;
+    }
+
+    function sort(products){
+        if(state.sortBy === "lowTohigh"){
+            return products.sort((a, b) => Number(a.price) - Number(b.price));
+        }
+        if(state.sortBy === "highToLow"){
+            return products.sort((a, b) => Number(b.price) - Number(a.price));
+        }
+
+        return products
+    }
+
+    function rating(products){
+        if(state.ratings === "4STARABOVE"){
+            return products.filter(product => product.rating >= 4);
+        }
+        if(state.ratings === "3STARABOVE"){
+            return products.filter(product => product.rating >= 3);
+        }
+        if(state.ratings === "2STARABOVE"){
+            return products.filter(product => product.rating >= 2);
+        }
+        if(state.ratings === "1STARABOVE"){
+            return products.filter(product => product.rating >= 1);
+        }
+
+        return products
+    }
+
+    const filteredProductList = rating(sort(inStock(bestSeller(state.productList))));
+
+     const value = {
+        state,
+        dispatch,
+        products: filteredProductList,
         initialProductList
     }
 
